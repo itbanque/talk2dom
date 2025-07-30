be389576b4d775038c14dc89f721af580fd37e4b
jian <fengjian1114@gmail.com>
2025-07-30 10:47:39 -0400

    change the allow_origins to variable

--- talk2dom/api/main.py
+++ talk2dom/api/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://talk2dom.itbanque.com"],
+    allow_origins=[os.environ.get("UI_DOMAIN")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
